914a298b9932addb33f6ecc1b18bcd65c5d7efb5
wyyishu2023 <wyyishu2023@163.com>
2025-03-12 22:30:12 +0800

    feat: 增加github地址方便下载全部

--- js/app.js
+++ js/app.js
@@ -734,6 +734,10 @@
 
   // 配置项
   const counterConfig = [
+    {
+      type: "github",
+      label: "https://github.com/wyyishu2023/uview-plus-offline-doc",
+    },
     { type: "site_uv", label: "访客数" },
     { type: "site_pv", label: "总访问量" },
     { type: "page_pv", label: "阅读量" },
@@ -765,10 +769,18 @@
     counterConfig.forEach((config) => {
       const item = document.createElement("span");
       item.className = "busuanzi-compact-item";
-      item.innerHTML = `
-      <span class="compact-label">${config.label}</span>
-      <span id="busuanzi_value_${config.type}" class="compact-value">0</span>
-    `;
+      if (config.type === "github") {
+        item.innerHTML = `
+          <a href="${config.label}" target="_blank" style="color: #1677ff;" title="获取离线版">
+            <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='black' d='M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12'/%3E%3C/svg%3E" alt="GitHub" >
+          </a>
+        `;
+      } else {
+        item.innerHTML = `
+        <span class="compact-label">${config.label}</span>
+        <span id="busuanzi_value_${config.type}" class="compact-value">0</span>
+      `;
+      }
       container.appendChild(item);
     });
 
